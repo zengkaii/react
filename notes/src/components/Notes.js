d23ff10020e92bfa72fed3f851e539455d827a1f
zengkai <1965599425@qq.com>
2018-07-18 17:08:41 +0800

    添加了react学习

--- notes/src/components/Notes.js
+++ notes/src/components/Notes.js
@@ -1,27 +1,77 @@
 import React, { Component } from 'react';
+import {db, loadCollection} from '../database'
 import Note from './Note';
 class Notes extends Component{
     // state 相当于vue中的data
+    constructor (props) {
+        super(props)
+        this.getInitialData()
+    }
+    getInitialData () {
+        loadCollection('notes')
+        .then(collection => {
+            // console.log(collection)
+            // collection.insert([
+            //     {
+            //         text:'看我'
+            //     },
+            //     {
+            //         text:'看你'
+            //     }
+            // ]);
+            // db.saveDatabase();
+            const entities = collection.chain()
+            .find()
+            .simplesort('$loki', 'isdesc')
+            .data()
+            // console.log(entities)
+            this.setState({
+                entities
+            })
+        })
+    }
     state = {
-        entities: [
-            '我很帅',
-            '我真的很帅'
-        ]
+        entities: []
     }
     createEntry () {
-        console.log(this)
+        // console.log(this)
+        loadCollection('notes')
+        .then((collection) => {
+            const entity = collection.insert({
+                text: ''
+            })
+            db.saveDatabase();
+            this.setState((preState) => {
+                const _entities = preState.entities;
+                _entities.unshift(entity);
+                return {
+                    entities: _entities
+                }
+            })
+        })
     }
     destoryEntity (entity) {
-        console.log(entity)
+        const _entities = this.state.entities.filter((_entity) => {
+            return _entity.$loki !== entity.$loki
+        })
+        this.setState({
+            entities: _entities
+        })
+        loadCollection('notes')
+        .then((collection) => {
+            collection.remove(entity)
+            db.saveDatabase()
+        })
     }
     render () {
         // react 独有的JSX 模板引擎
         // 在js 里直接写html
         // react className
         // html -> js node 是会编译成为js的 ， class是一个关键字
+        // key值的选择 在删除的时候  使用entity.$loki
         const entities = this.state.entities;
         const noteItems = entities.map((entity,index) => {
-            return <Note key= {index} entity = {entity} destoryEntity = {this.destoryEntity}/>
+            return <Note key= {entity.$loki} entity = {entity} destoryEntity = {this.destoryEntity.bind(this)}/>
         })
         return (
             <div className="ui container notes">
