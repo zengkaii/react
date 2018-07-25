8347f628c852146b392a26d3140615fc2c734514
zengkai <1965599425@qq.com>
2018-07-25 14:35:46 +0800

    react-music-jsonp

---
+++ music-react/src/components/recommend/Recommend.js
@@ -0,0 +1,62 @@
+import Swiper from 'swiper';
+import 'swiper/dist/css/swiper.css';
+import React, { Component } from 'react';
+import { getCarousel } from '@/api/recommend';
+import './recommend.styl';
+import { CODE_SUCCESS } from '../../api/config';
+
+class Recommend extends Component {
+    constructor (props) {
+        super(props);
+        this.state = {
+            sliderList: []
+        }
+    }
+    // ajax 有时会阻塞页面，应在挂载之后请求数据
+    componentDidMount() {
+        getCarousel().then(res => {
+            // console.log(res);
+            if(res) {
+                if(res.code === CODE_SUCCESS) {
+                    this.setState({
+                        sliderList: res.data.slider
+                    }, () => {
+                        if (!this.silderSwiper) {
+                            this.silderSwiper = new Swiper('.slider-container', {
+                                loop: true,
+                                autoplay: 3000,
+                                autoplayDisableOnInteraction: false,
+                                pagination: '.swiper-pagination'
+                            })
+                        }
+
+                    })
+                }
+            }
+        })
+    }
+    render() {
+        return (
+            <div className="music-recommend">
+                <div className="slider-container">
+                    <div className="swiper-wrapper">
+                        {
+                            this.state.sliderList.map(slider => {
+                                return (
+                                    <div className="swiper-slide" key={slider.id}>
+                                        <a href="#" className="slider-nav">
+                                            <img src={slider.picUrl} width="100%" height="100%"  alt="推荐" />
+                                        </a>
+                                    </div>
+                                )
+                            })
+                        }
+                    </div>
+                    <div className="swiper-pagination"></div>
+                </div>
+            </div>
+        )
+    }
+};
+
+export default Recommend;
