5a8dd6578ce3fdb3bda957cf22e412605082ddc2
zengkai <1965599425@qq.com>
2018-07-25 17:14:20 +0800

    react-music_better-scroll

---
+++ music-react/src/common/scroll/Scroll.js
@@ -0,0 +1,56 @@
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
+import BScroll from 'better-scroll';
+import './Scroll.styl';
+class Scroll extends Component {
+    
+    componentDidUpdate () {
+        // 组件更新后
+        if (this.bScroll && this.props.refresh === true) {
+            this.bScroll.refresh();
+        }
+    }
+    // 停止scroll,减少内存消耗
+    componentWillUnmount () {
+        this.bScroll.off('scroll');
+        this.bScroll = null
+    }
+    componentDidMount () {
+        this.scrollView = ReactDOM.findDOMNode(this.refs.scrollView);
+        if (!this.bScroll) {
+            this.bScroll = new BScroll(this.scrollView, {
+                probeType: 3,
+                click: this.props.click
+            });
+            if (this.props.onScroll) {
+                this.bScroll.on('scroll', (scroll) => {
+                    this.props.onScroll(scroll);
+                })
+            }
+        }
+    }
+    refresh () {
+        if (this.bScroll) {
+            this.bScroll.refresh();
+        }
+    }
+    render () {
+        return (
+            <div className="scroll-view" ref="scrollView">
+                {this.props.children}
+            </div>
+        )
+    }
+}
+Scroll.defaultProps = {
+    click: true,
+    refresh: false,
+    onScroll: null
+}
+Scroll.propTypes = {
+    click: PropTypes.bool,
+    refresh: PropTypes.bool,
+    onScroll:PropTypes.func
+}
+export default Scroll;
--- music-react/src/components/recommend/Recommend.js
+++ music-react/src/components/recommend/Recommend.js
@@ -4,12 +4,16 @@ import React, { Component } from 'react';
 import { getCarousel } from '@/api/recommend';
 import './recommend.styl';
 import { CODE_SUCCESS } from '../../api/config';
-
+import { getNewAlbum } from '../../api/recommend';
+import * as AlbumModel from '@/model/album';
+import Scroll from '@/common/scroll/Scroll'
 class Recommend extends Component {
     constructor (props) {
         super(props);
         this.state = {
-            sliderList: []
+            sliderList: [],
+            newAlbums: [],
+            refreshScroll: false
         }
     }
     // ajax 有时会阻塞页面，应在挂载之后请求数据
@@ -34,26 +38,87 @@ class Recommend extends Component {
                 }
             }
         })
+        getNewAlbum().then(res => {
+            if (res) {
+                if (res.code === CODE_SUCCESS) {
+                    let albumList = res.albumlib.data.list;
+                    // console.log(albumList)
+                    albumList.sort((a, b) => {
+                        return new Date(b.public_time).getTime() - new Date(a.public_time).getTime();
+                    })
+                    console.log(albumList)
+                    this.setState({
+                        newAlbums:albumList
+                    }, () => {
+                        this.setState({
+                            refreshScroll:true
+                        })
+                    })
+                }
+            }
+        })
+    }
+    toLink (linkUrl) {
+        // 使用箭头函数归并this指向问题
+        return () => {
+            // console.log(this)
+            window.location.href = linkUrl
+        }
     }
     render() {
+        const albums = this.state.newAlbums.map(item => {
+            const album = AlbumModel.createAlbumByList(item)
+            return (
+  
+                <div className="album-wrapper" key={album.id}>
+                
+                    <div className="left">
+                        <img src={album.img} alt={album.name} width="100%" height="100%"/>
+                    </div>
+                    <div className="right">
+                        <div className="album-name">
+                            {album.name}
+                        </div>
+                        <div className="singer-name">
+                            {album.singer}
+                        </div>
+                        <div className="public-name">
+                            {album.publicTime}
+                        </div>
+                    </div>
+                
+                </div>
+            )
+        })
         return (
             <div className="music-recommend">
-                <div className="slider-container">
-                    <div className="swiper-wrapper">
-                        {
-                            this.state.sliderList.map(slider => {
-                                return (
-                                    <div className="swiper-slide" key={slider.id}>
-                                        <a href="#" className="slider-nav">
-                                            <img src={slider.picUrl} width="100%" height="100%"  alt="推荐" />
-                                        </a>
-                                    </div>
-                                )
-                            })
-                        }
+            <Scroll refresh={this.state.refreshScroll}>
+                <div>
+                    <div className="slider-container">
+                        <div className="swiper-wrapper">
+                            {
+                                this.state.sliderList.map(slider => {
+                                    return (
+                                        <div className="swiper-slide" key={slider.id}>
+                                            <a onClick={this.toLink(slider.linkUrl)}  className="slider-nav">
+                                                <img src={slider.picUrl} width="100%" height="100%"  alt="推荐" />
+                                            </a>
+                                        </div>
+                                    )
+                                })
+                            }
+                        </div>
+                        <div className="swiper-pagination"></div>
                     </div>
-                    <div className="swiper-pagination"></div>
+                    <div className="album-container">
+                        <h1 className="title">最新专辑</h1>
+                        <div className="album-list">
+                            {albums}
+                        </div>
+                    </div>
                 </div>
+            </Scroll>
+
             </div>
         )
     }
